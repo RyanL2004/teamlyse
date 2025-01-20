9b0679e733232e47776ca10fe85fdcf3a1c70c80
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-20 16:04:11 +0000

    Add PetSelectionComponent to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PetSelection from './components/PetSelection'
 
-
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [selectedPet, setSelectedPet] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div>
+    <h1>Welcome to MeeTPeT</h1>
+    <PetSelection onSelectPet={setSelectedPet} />
+    {selectedPet && (
+        <p style={{ textAlign: 'center' }}>
+            Your pet, {selectedPet.name}, will assist you during meetings!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )}
+</div>
   )
-}
+};
 
 export default App
