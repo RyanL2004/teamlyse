b8d4e99b6e70791df01878e156d5bb151982b216
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-01 13:14:41 +0000

    feat: components routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from 'react'
-import PetSelection from './components/PetSelection'
+import { useState } from "react";
+import PetSelection from "./components/PetSelection";
+import Layouts from "./components/Layouts";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./components/Home";
 
-function App () {
+function App() {
   const [selectedPet, setSelectedPet] = useState(null);
 
   return (
     <div>
-    <h1>Welcome to MeeTPeT</h1>
-    <PetSelection onSelectPet={setSelectedPet} />
-    {selectedPet && (
-        <p style={{ textAlign: 'center' }}>
-            Your pet, {selectedPet.name}, will assist you during meetings!
-        </p>
-    )}
-</div>
-  )
-};
+      <Layouts>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/signin" element={<SignIn />}></Route>
+        <Route exact path="signup" element={<SignUp />}></Route>
+        <Route exact path="/petselection" element={<PetSelection />}></Route>
+      </Routes>
+      </Layouts>
 
-export default App
+      {/*
+      <PetSelection onSelectPet={setSelectedPet} />
+      {selectedPet && (
+        <p style={{ textAlign: "center" }}>
+          Your pet, {selectedPet.name}, will assist you during meetings!
+        </p>
+      )}
+      */}
+
+    </div>
+  );
+}
+
+export default App;
