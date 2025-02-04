57cf5bf5f4f0d427d6c5336097a5ad4ed4cf0d94
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-04 18:20:39 +0000

    feat: Dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PetSelection from "./components/PetSelection";
+import LiveCompanion from "./pages/LiveCompanion";
 import Layouts from "./components/Layouts";
 import {
   BrowserRouter as Router,
@@ -13,6 +13,9 @@ import Home from "./components/Home";
 import Features from "./pages/Features";
 import Dashboard from "./pages/Dashboard";
 import Pricing from "./pages/Pricing";
+import MeetingsHistory from "./pages/MeetingsHistory";
+import LiveMeeting from "./pages/LiveMeeting";
+import UpcomingMeetings from "./pages/UpcomingMeetings";
 function App() {
   const [selectedPet, setSelectedPet] = useState(null);
 
@@ -23,10 +26,15 @@ function App() {
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/signin" element={<SignIn />}></Route>
         <Route exact path="/signup" element={<SignUp />}></Route>
-        <Route exact path="/petselection" element={<PetSelection />}></Route>
+        <Route exact path="/live-companion" element={<LiveCompanion />}></Route>
         <Route exact path="/features" element={<Features />}></Route>
         <Route exact path="/dashboard" element={<Dashboard />}></Route>
         <Route exact path="/pricing" element={<Pricing />}></Route>
+        <Route exact path="live-meeting" element={<LiveMeeting />}></Route>
+        <Route exact path="upcoming-meetings" element={<UpcomingMeetings />}></Route>
+        <Route exact path="meetings-history" element={<MeetingsHistory />}></Route>
+        
+
       </Routes>
       </Layouts>
 
