ad7d9f91f0698d51ce9aff10bd7b675fd3e48220
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-06 18:05:01 +0000

    Chore: use Pet route & middleware specification

--- server/server.js
+++ server/server.js
@@ -5,11 +5,10 @@ import router from './routes/testRoute.js';
 import petRoute from './routes/petRoute.js';
 import connectDB from './config/db.js';
 
-
 dotenv.config();
 const app = express();
 
-// Middleware
+// Middleware : Needs to be pushed away in a future release for readability and performance
 app.use(cors());
 app.use(express.json());
 connectDB();
@@ -18,6 +17,8 @@ connectDB();
 app.use('/api', router);
 // Pet Route
 app.use('/api/pets', petRoute);
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
