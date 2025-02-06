33e2704eb6b139c468643f0f0ff7801319fc7298
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-06 03:12:08 +0000

    chore: Structured main app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -17,9 +18,26 @@ import MeetingsHistory from "./pages/MeetingsHistory";
 import LiveMeeting from "./pages/LiveMeeting";
 import UpcomingMeetings from "./pages/UpcomingMeetings";
 import { SidebarProvider } from "./context/SidebarContext";
+import { AnimatePresence } from "framer-motion"; //Permit smooth transition between dashboard component pages
+
+function AnimatedRoutes() {
+  const location = useLocation(); // Detect current page
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/live-meeting" element={<LiveMeeting />} />
+        <Route exact path="/upcoming-meetings" element={<UpcomingMeetings />} />
+        <Route exact path="/meetings-history" element={<MeetingsHistory />} />
+        <Route exact path="/live-companion" element={<LiveCompanion />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   const [selectedPet, setSelectedPet] = useState(null);
-
   return (
     <div>
       
@@ -30,15 +48,12 @@ function App() {
       <Route exact path="/signin" element={<Layouts><SignIn /></Layouts>} />
       <Route exact path="/signup" element={<Layouts><SignUp /></Layouts>} />
       </Routes>
+      
+      
       <SidebarProvider>
-      <Routes>
-      <Route exact path="/dashboard" element={<Dashboard />}></Route>
-      <Route exact path="/live-meeting" element={<LiveMeeting />}></Route>
-      <Route exact path="/upcoming-meetings" element={<UpcomingMeetings />}></Route>
-      <Route exact path="/meetings-history" element={<MeetingsHistory />}></Route>
-      <Route exact path="/live-companion" element={<LiveCompanion />}></Route>
-      </Routes>
+        <AnimatedRoutes />
       </SidebarProvider>
+      
       
 
       {/*
