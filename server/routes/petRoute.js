eaa1eefe720de1b2b0ec143d36d555594b65178f
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-11 14:16:25 +0000

    Refactor: save-selection end-point from petRoute as it is related to the user CRUD logic

--- server/routes/petRoute.js
+++ server/routes/petRoute.js
@@ -3,34 +3,7 @@ import Pet from '../models/Pet.js';
 
 const petRoute = express.Router();
 
-// Saves Pet/Compagnion Selection
-petRoute.post('/save-selection', async (req, res) => {
-    const { pet } = req.body;
-
-    if(!pet){
-        return res.status(400).send({error: 'Pet selection is required'});
-    }
-
-    try {
-        // Check if pet already exists (to avoid duplicated IDs)
-        const existingPet = await Pet.findOne({ id: pet.id });
-
-        if(existingPet) {
-            return res.status(409).json({message: `Pet ${pet.name} is already selected`})
-        }
-
-        //Save new pet Selection
-        const newPet = newPet(pet);
-        const savedPet = await newPet.save();
-        res.status(200).json({message: `pet ${pet.name} selected successfully!`, pet: savedPet});
-
-    }
-    catch (error) {
-        res.status(500).json({error:'Failed to save selected pet'});
-    }  
-});
-
-// Fetch all the pets available in Mongoose db
+// Fetch all the pets available in DB
 petRoute.get('/all', async (req, res) => {
     try {
         const pets = await Pet.find();
