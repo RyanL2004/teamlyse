9e0728a8917b527bcd8c126cdbcaf61db947d0a7
RyanL2004 <rayanlouahche2004@gmail.com>
2025-02-21 02:28:25 +0000

    feat: Carousel in Home component

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -7,16 +7,16 @@ import { Link } from "react-router-dom";
 const Carousel = () => {
   // Define your items data (this can come from props or state)
   const items = [
-    { src: "/public/assets/images/dragon_1.jpg", position: 1 },
-    { src: "/public/assets/images/dragon_2.jpg", position: 2 },
-    { src: "/public/assets/images/dragon_3.jpg", position: 3 },
-    { src: "/public/assets/images/dragon_4.jpg", position: 4 },
-    { src: "/public/assets/images/dragon_6.jpg", position: 6 },
-    { src: "/public/assets/images/dragon_7.jpg", position: 7 },
-    { src: "/public/assets/images/dragon_8.jpg", position: 8 },
-    { src: "/public/assets/images/dragon_9.jpg", position: 9 },
-    { src: "/public/assets/images/dragon_10.jpg", position: 10 },
-    { src: "/public/assets/images/dragon_5.jpg", position: 5 },
+    { src: "/assets/images/dragon_1.jpg", position: 1 },
+    { src: "/assets/images/dragon_2.jpg", position: 2 },
+    { src: "/assets/images/dragon_3.jpg", position: 3 },
+    { src: "/assets/images/dragon_4.jpg", position: 4 },
+    { src: "/assets/images/dragon_6.jpg", position: 6 },
+    { src: "/assets/images/dragon_7.jpg", position: 7 },
+    { src: "/assets/images/dragon_8.jpg", position: 8 },
+    { src: "/assets/images/dragon_9.jpg", position: 9 },
+    { src: "/assets/images/dragon_10.jpg", position: 10 },
+    { src: "/assets/images/dragon_5.jpg", position: 5 },
   ];
 
   return (
@@ -43,7 +43,7 @@ const Carousel = () => {
           <p>
           Enhance your professional meetings experience with AI
           </p>
-          <Link to="/pricing">
+          <Link to="/getstarted">
           <button className="rounded-full py-2 px-5 bg-white text-black font-semibold mt-5 transition-transform duration-200 hover:scale-110 hover:shadow-lg hover:bg-gray-300">
             Get Started
           </button>
